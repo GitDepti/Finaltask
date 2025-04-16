1026135a21552578ffce6750cfc8deed775b5207
Deepthi <deepthirao345@gmail.com>
2025-04-16 14:45:55 +0530

    FinalChanges

--- src/test/java/stepdefinitions/ConduitStepdef.java
+++ src/test/java/stepdefinitions/ConduitStepdef.java
@@ -39,13 +39,7 @@ public void user_should_be_on_new_article_page(DataTable userData) {
 }
 @When("User enters  Article details")
 public void user_enters_article_details(io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
+    
     throw new io.cucumber.java.PendingException();
 }
 @Then("Article will be created")
